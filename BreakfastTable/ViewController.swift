0a4f8500ed6c3c3506fd809322338f5e73a10f29
Steve Lipton <steve@makeapppie.com>
2017-12-05 06:30:58 -0600

    initial commit of begin file

--- BreakfastTable/ViewController.swift
+++ BreakfastTable/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    
+    var breakfastStuff = ["French Toast","Pancakes"]
+
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return breakfastStuff.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = breakfastStuff[row]
+        if row % 2 == 0 {
+            cell.backgroundColor = .blue
+        } else {
+            cell.backgroundColor = .yellow
+        }
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = indexPath.row
+        let displayVC = DisplayViewController()
+        displayVC.title = breakfastStuff[row]
+        navigationController?.pushViewController(displayVC, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Breakfast"
     }
 
     override func didReceiveMemoryWarning() {
